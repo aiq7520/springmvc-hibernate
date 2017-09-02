600a7551698784ffe3d7d11b69796c707d1c9ff2
aiq7520 <1748288924@qq.com>
2017-09-02 14:36:36 +0800

    Revert "Revert "还原项目""
    This reverts commit 402add0882ad458825f7926a8418640e1e42af32.

---
+++ springmvc+hibernate(170902)/src/test/java/ssm/dao/DaoTest.java
@@ -0,0 +1,43 @@
+package ssm.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ssm.entity.Department;
+import ssm.entity.Emp;
+import ssm.service.Service;
+
+@ContextConfiguration("/applicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DaoTest {
+	//@Autowired
+	//private EmpDao empDao;
+	@Autowired
+	private Service empService;
+	//@Autowired
+	//private HibernateTemplate hibernateTemplate;
+	
+	
+	@Test//创建员工
+	public void testSave(){
+		for(int i=0;i<1;i++){
+			Emp e= new Emp("张三"+i,22,10000);
+			Department d = new Department();
+			d.setId(1);
+			e.setDepartment(d);
+			empService.add(e);
+		}
+	}
+	@Test//创建部门
+	public void testSaveD(){
+			Department d1= new Department("人事部");
+			Department d2= new Department("开发部");
+			Department d3= new Department("财务部");
+			empService.add(d1);
+			empService.add(d2);
+			empService.add(d3);
+	}
+}
